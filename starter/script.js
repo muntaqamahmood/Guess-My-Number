9b0c325821ac81f9f749bfb979d5eab2a09550e2
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2024-02-07 23:13:23 -0500

    finish: done app

--- starter/script.js
+++ starter/script.js
@@ -4,12 +4,12 @@ function resetGame() {
   secretNumber = Math.floor(Math.random() * 20);
   const checkBtn = document.querySelector('.btn.check');
   checkBtn.disabled = false;
-
   tries = 20;
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.guess').value = '';
   document.querySelector('.score').innerHTML = 20;
   document.querySelector('.message').innerHTML = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
 }
 window.onload = function () {
   secretNumber = Math.floor(Math.random() * 20);
@@ -22,10 +22,14 @@ window.onload = function () {
     console.log(checkBtn.disabled);
     const guessedVal = Number(guess.value); // input.value
     const hs = document.querySelector('.highscore').innerHTML;
-    if (!guessedVal)
+    // no input
+    if (!guessedVal) {
       document.querySelector('.message').innerHTML = '⛔ No Number!';
+    }
+    // correct
     else if (guessedVal === secretNumber) {
       tries--;
+      // highscore logic
       if (tries > Number(hs)) {
         document.querySelector('.highscore').innerHTML = tries;
       }
@@ -36,14 +40,31 @@ window.onload = function () {
       //   tries = 20;
       document.querySelector('.score').innerHTML = tries;
       checkBtn.disabled = true;
-    } else if (guessedVal > secretNumber) {
-      tries--;
-      document.querySelector('.message').innerHTML = 'You guessed too HIGH!';
-      document.querySelector('.score').innerHTML = tries;
-    } else {
-      tries--;
-      document.querySelector('.message').innerHTML = 'You guessed too LOW!';
-      document.querySelector('.score').innerHTML = tries;
+      document.querySelector('.number').textContent = secretNumber;
+    }
+    // too high guess
+    else if (guessedVal > secretNumber) {
+      if (tries > 1) {
+        tries--;
+        document.querySelector('.message').innerHTML = 'You guessed too HIGH!';
+        document.querySelector('.score').innerHTML = tries;
+      } else {
+        document.querySelector('.message').innerHTML =
+          'You are out of tries! Try Again!';
+        document.querySelector('.score').innerHTML = tries;
+      }
+    }
+    // too low guess
+    else {
+      if (tries > 1) {
+        tries--;
+        document.querySelector('.message').innerHTML = 'You guessed too LOW!';
+        document.querySelector('.score').innerHTML = tries;
+      } else {
+        document.querySelector('.message').innerHTML =
+          'You are out of tries! Try Again!';
+        document.querySelector('.score').innerHTML = tries;
+      }
     }
   });
   document.querySelector('.btn.again').addEventListener('click', function () {
