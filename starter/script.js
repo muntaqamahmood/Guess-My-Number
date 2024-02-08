03e3b8ffeddf05d57f0bd3b843a72ec74da410ac
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2024-02-07 23:05:49 -0500

    fix: reset game logic

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,7 @@
 let tries = 20;
-let secretNumber = Math.floor(Math.random() * 20);
+let secretNumber = 0;
 function resetGame() {
+  secretNumber = Math.floor(Math.random() * 20);
   const checkBtn = document.querySelector('.btn.check');
   checkBtn.disabled = false;
 
@@ -11,8 +12,9 @@ function resetGame() {
   document.querySelector('.message').innerHTML = 'Start guessing...';
 }
 window.onload = function () {
+  secretNumber = Math.floor(Math.random() * 20);
   // Your code here
-  'use strict';
+  ('use strict');
   console.log(secretNumber);
   const guess = document.querySelector('.guess');
   const checkBtn = document.querySelector('.btn.check');
@@ -20,7 +22,9 @@ window.onload = function () {
     console.log(checkBtn.disabled);
     const guessedVal = Number(guess.value); // input.value
     const hs = document.querySelector('.highscore').innerHTML;
-    if (guessedVal === secretNumber) {
+    if (!guessedVal)
+      document.querySelector('.message').innerHTML = '⛔ No Number!';
+    else if (guessedVal === secretNumber) {
       tries--;
       if (tries > Number(hs)) {
         document.querySelector('.highscore').innerHTML = tries;
